24f08f3e62e8c7bc498a64d62e1d08b8da43a528
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-08-19 15:26:15 +0200

    Add icon

--- src/Theme/head.tpl.php
+++ src/Theme/head.tpl.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title><?= $this->getTitle(); ?></title>
+    <link rel="icon" type="image/x-icon" href="<?= $this->base; ?>/img/favicon.ico">
     <link rel="stylesheet" type="text/css" href="<?= $this->base; ?>/css/styles.css">
     <script type="text/javascript">var BASE = '<?= $this->base; ?>';</script>
 </head>
