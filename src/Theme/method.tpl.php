19d4e4635d430bdc654c0221c19161a2c6162f86
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-10-20 15:46:47 +0200

    Remove author

--- src/Theme/method.tpl.php
+++ src/Theme/method.tpl.php
@@ -1,6 +1,6 @@
 <?php include 'header.tpl.php'; ?>
 <h1><?= $this->ref->getDeclaringClass()->getShortName(); ?> ~ <?= $this->ref->getShortName(); ?></h1>
-<p><?= $this->getComment()->isDeprecated() ? '<span class="deprecated">@deprecated</span> ' : ''; ?>@since: <?= $this->getComment()->getSince(); ?>; @author: <?= $this->getComment()->getAuthor(); ?>; @class: <?= $this->getClassLink(); ?></p>
+<p><?= $this->getComment()->isDeprecated() ? '<span class="deprecated">@deprecated</span> ' : ''; ?>@since: <?= $this->getComment()->getSince(); ?>; @class: <?= $this->getClassLink(); ?></p>
 <h2>Function</h2>
 <pre><?= $this->getMethod(); ?></pre>
 <?php $description = $this->getComment()->getDescription(); if(!empty($description)) : ?>
