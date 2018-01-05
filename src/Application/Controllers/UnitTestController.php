1a6a9e84b233b822ed438c8bb9215a2c8b41eb8d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-01-05 12:44:03 +0100

    Remove framework dependency

--- src/Application/Controllers/UnitTestController.php
+++ src/Application/Controllers/UnitTestController.php
@@ -3,7 +3,6 @@
 namespace Documentor\src\Application\Controllers;
 
 use Documentor\src\Application\Views\TestView;
-use phpOMS\System\File\Local\File;
 
 class UnitTestController
 {
@@ -74,7 +73,7 @@ class UnitTestController
         $testView->setSection('Test');
         $testView->setTest($this->test);
         $testView->setResults($this->testResults);
-
-        File::put($testView->getPath(), $testView->render());
+        
+        file_put_contents($testView->getPath(), $testView->render());
     }
 }
