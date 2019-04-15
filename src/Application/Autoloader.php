723c57a71ddcead9ead373c5a7959a9eab7455fa
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-04-15 21:51:36 +0200

    Update Autoloader.php

--- src/Application/Autoloader.php
+++ src/Application/Autoloader.php
@@ -1,32 +1,8 @@
 <?php
-/**
- * Orange Management
- *
- * PHP Version 7.1
- *
- * @category   TBD
- * @package    TBD
- * @author     OMS Development Team <dev@oms.com>
- * @copyright  2013 Dennis Eichhorn
- * @license    OMS License 1.0
- * @version    1.0.0
- * @link       http://orange-management.com
- */
-
 namespace Documentor\src\Application;
 
 spl_autoload_register('\Documentor\src\Application\Autoloader::defaultAutoloader');
 
-/**
- * Autoloader class.
- *
- * @category   Framework
- * @package    Framework
- * @author     OMS Development Team <dev@oms.com>
- * @license    OMS License 1.0
- * @link       http://orange-management.com
- * @since      1.0.0
- */
 class Autoloader
 {
 
