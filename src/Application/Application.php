9ffcd78ab6a7ef801c4db1757bae58e1a286f041
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-01-26 17:32:13 +0100

    Update Application.php

--- src/Application/Application.php
+++ src/Application/Application.php
@@ -20,48 +20,48 @@ class Application
     {
         $this->setupHandlers();
 
-        $help        = ($key = array_search('-h', $argv)) === false || $key === count($argv) - 1 ? null : trim($argv[$key + 1], '" ');
-        $source      = ($key = array_search('-s', $argv)) === false || $key === count($argv) - 1 ? null : trim($argv[$key + 1], '" ');
-        $destination = ($key = array_search('-d', $argv)) === false || $key === count($argv) - 1 ? null : trim($argv[$key + 1], '" ');
+        $help        = ($key = \array_search('-h', $argv)) === false || $key === \count($argv) - 1 ? null : \trim($argv[$key + 1], '" ');
+        $source      = ($key = \array_search('-s', $argv)) === false || $key === \count($argv) - 1 ? null : \trim($argv[$key + 1], '" ');
+        $destination = ($key = \array_search('-d', $argv)) === false || $key === \count($argv) - 1 ? null : \trim($argv[$key + 1], '" ');
 
         if (isset($help) || !isset($source) || !isset($destination)) {
             $this->printUsage();
         } else {
-            $destination = rtrim($destination, '/\\');
-            $source      = rtrim($source, '/\\');
+            $destination = \rtrim($destination, '/\\');
+            $source      = \rtrim($source, '/\\');
             $this->createDocumentation($source, $destination, $argv);
         }
     }
 
     private function setupHandlers()
     {
-        set_exception_handler(function(\Throwable $e) { 
+        \set_exception_handler(function(\Throwable $e) { 
             echo $e->getLine(), ': ' , $e->getMessage(); 
         });
 
-        set_error_handler(function(int $errno, string $errstr, string $errfile, int $errline) { 
-            if (!(error_reporting() & $errno)) {
+        \set_error_handler(function(int $errno, string $errstr, string $errfile, int $errline) { 
+            if (!(\error_reporting() & $errno)) {
                 echo $errline , ': ' , $errfile;
             } 
         });
 
-        register_shutdown_function(function() { 
-            $e = error_get_last(); 
+        \register_shutdown_function(function() { 
+            $e = \error_get_last(); 
             
             if (isset($e)) {
                 echo $e['line'] , ': ' , $e['message']; 
             }
         });
 
-        mb_internal_encoding('UTF-8');
+        \mb_internal_encoding('UTF-8');
     }
 
     private function createDocumentation(string $source, string $destination, array $argv)
     {
-        $unitTest     = ($key = array_search('-u', $argv)) === false || $key === count($argv) - 1 ? null : trim($argv[$key + 1], '" ');
-        $codeCoverage = ($key = array_search('-c', $argv)) === false || $key === count($argv) - 1 ? null : trim($argv[$key + 1], '" ');
-        $guide        = ($key = array_search('-g', $argv)) === false || $key === count($argv) - 1 ? null : trim($argv[$key + 1], '" ');
-        $base         = ($key = array_search('-b', $argv)) === false || $key === count($argv) - 1 ? $destination : trim($argv[$key + 1], '" ');
+        $unitTest     = ($key = \array_search('-u', $argv)) === false || $key === \count($argv) - 1 ? null : \trim($argv[$key + 1], '" ');
+        $codeCoverage = ($key = \array_search('-c', $argv)) === false || $key === \count($argv) - 1 ? null : \trim($argv[$key + 1], '" ');
+        $guide        = ($key = \array_search('-g', $argv)) === false || $key === \count($argv) - 1 ? null : \trim($argv[$key + 1], '" ');
+        $base         = ($key = \array_search('-b', $argv)) === false || $key === \count($argv) - 1 ? $destination : \trim($argv[$key + 1], '" ');
         $base         = rtrim($base, '/\\');
         $sources      = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($source));
 
@@ -91,8 +91,8 @@ class Application
     {
         foreach ($sources as $source) {
             if ($source->isFile() 
-                && (($temp = strlen($source->getPathname()) - strlen('.php')) >= 0 && strpos($source->getPathname(), '.php', $temp) !== false)
-                && (stripos($source->getPathname(), '/test') === false && stripos($source->getPathname(), '\\test') === false)
+                && (($temp = \strlen($source->getPathname()) - strlen('.php')) >= 0 && \strpos($source->getPathname(), '.php', $temp) !== false)
+                && (\stripos($source->getPathname(), '/test') === false && \stripos($source->getPathname(), '\\test') === false)
             ) {
                 $this->docController->parse($source);
             }
